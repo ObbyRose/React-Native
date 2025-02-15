a62d1f7c5fb0506e098f775da3248c8ee9312eb8
Tal Maman <dreapet41s@gmail.com>
2025-02-15 17:15:30 +0200

    Replace explore.tsx with explore.jsx and implement new Explore component with image background

---
+++ lesson01/app/(tabs)/explore.jsx
@@ -0,0 +1,38 @@
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import React from 'react'
+
+import coffeeBeans from "@/assets/images/coffee-splash.png"
+
+const Explore = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={coffeeBeans} style={styles.image}>
+        <Text style={styles.text}>In our coffee shop we use jamaican coffee beans that had been roasted to perfection</Text>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default Explore
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image:{
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  }
+})
