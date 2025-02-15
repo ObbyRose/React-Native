9c358f78f130cf5fc48670db55312802e1da45d9
Tal Maman <dreapet41s@gmail.com>
2025-02-15 17:24:34 +0200

    Refactor tab layout and replace index and explore components with new implementations

--- lesson01/app/index.jsx
+++ lesson01/app/index.jsx
@@ -1,12 +1,14 @@
 import { View, Text, StyleSheet, ImageBackground } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 import icedCoffeeImg from '@/assets/images/iced-coffee.png'
 const app = () => {
   return (
     <View style={styles.container}>
       <ImageBackground source={icedCoffeeImg} resizeMode="cover" style={styles.image}>
-        <Text style={styles.text}>Coffee Shop</Text>
+        <Text style={styles.title}>Coffee Shop</Text>
+        <Link href={"/explore"} style={styles.link}> Explore </Link>
       </ImageBackground>
     </View>
   )
@@ -28,11 +30,20 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     justifyContent: 'center',
   },
-  text: {
+  title: {
     color: 'white',
     fontSize: 42,
     fontWeight: 'bold',
     textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    marginBottom: 120,
+  },
+  link: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textDecorationLine: 'underline',
     backgroundColor: 'rgba(0,0,0,0.5)',
   }
 })
